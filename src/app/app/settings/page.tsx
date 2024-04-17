6e50479808cec082faeee6a6c22d8878a64b5e97
rodrigosilverioKenta <rodrigo-silverio@kenta.com.br>
2024-04-17 10:09:48 -0300

    settings project

---
+++ src/app/app/settings/page.tsx
@@ -0,0 +1,10 @@
+
+
+
+export default function Page() {
+  return (
+    <main className="flex items-center justify-center h-screen">
+      <h1>settings</h1>
+    </main>
+  )
+}
---
+++ src/components/logo.tsx
@@ -0,0 +1,9 @@
+import { RocketIcon } from '@radix-ui/react-icons'
+
+export function Logo() {
+  return (
+    <div className="bg-primary h-8 w-8 flex items-center rounded-md justify-center">
+      <RocketIcon className="w-5 h-5 text-primary-foreground" />
+    </div>
+  )
+}
